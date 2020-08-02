3c018677d332bb20f912dde53adeed31f4c11b4f
trung nguyen <trungnguyenthanh70@gmail.com>
2020-08-02 12:06:40 +0700

    Update slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $('.app-hero').slick({
         Infinity:true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+      arrows: true,
+      dots:true,
     });
   });
