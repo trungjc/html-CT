3f6936359b56fded1f5ecd5851a80f00062fbd41
ntd210800 <ntd210800@gmail.com>
2020-08-02 12:04:43 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    console.log("hello world")
+    $('.app-hero').slick({
+        Infinity:true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+  });
